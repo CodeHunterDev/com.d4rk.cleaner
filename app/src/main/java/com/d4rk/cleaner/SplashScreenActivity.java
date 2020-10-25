8aad3d0a5507c56af074c202e2a0775b55950ea8
D4rK <61864357+D4rK7355608@users.noreply.github.com>
2020-10-25 13:52:14 +0200

    Cleaner!

--- app/src/main/java/com/d4rk/cleaner/SplashScreenActivity.java
+++ app/src/main/java/com/d4rk/cleaner/SplashScreenActivity.java
@@ -1,14 +1,16 @@
 package com.d4rk.cleaner;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashScreenActivity extends AppCompatActivity {
-    int secondsDelayed = 5;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        Intent i = new Intent(SplashScreenActivity.this, MainActivity.class);
+        startActivity(i);
     }
 }
