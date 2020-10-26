f38912b31b255b0f8a1056c4d86e8958f77430c0
D4rK <61864357+D4rK7355608@users.noreply.github.com>
2020-10-26 10:18:42 +0200

    Cleaner!

--- app/src/main/java/com/d4rk/cleaner/SplashScreenActivity.java
+++ app/src/main/java/com/d4rk/cleaner/SplashScreenActivity.java
@@ -1,16 +1,41 @@
 package com.d4rk.cleaner;
 
 import android.content.Intent;
+import android.os.Handler;
+import android.app.Activity;
 import android.os.Bundle;
 
-import androidx.appcompat.app.AppCompatActivity;
+public class SplashScreenActivity extends Activity {
+    private final Handler mWaitHandler = new Handler();
 
-public class SplashScreenActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-        Intent i = new Intent(SplashScreenActivity.this, MainActivity.class);
-        startActivity(i);
+
+        mWaitHandler.postDelayed(() -> {
+
+            //The following code will execute after the 5 seconds.
+
+            try {
+
+                //Go to next page i.e, start the next activity.
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+
+                //Let's Finish Splash Activity since we don't want to show this when user press back button.
+                finish();
+            } catch (Exception exception) {
+                exception.printStackTrace();
+            }
+        }, 2000);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        //Remove all the callbacks otherwise navigation will execute even after activity is killed or closed.
+        mWaitHandler.removeCallbacksAndMessages(null);
     }
 }
