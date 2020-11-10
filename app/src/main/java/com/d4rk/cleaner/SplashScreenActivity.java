2d197fe72582c180c2c9fbbcaad1e0b5386b86fa
D4rK <61864357+D4rK7355608@users.noreply.github.com>
2020-11-10 17:36:06 +0200

    Fixed splash screen.

--- app/src/main/java/com/d4rk/cleaner/SplashScreenActivity.java
+++ app/src/main/java/com/d4rk/cleaner/SplashScreenActivity.java
@@ -1,27 +1,25 @@
 package com.d4rk.cleaner;
 import android.content.Intent;
-import android.os.Handler;
-import android.app.Activity;
+import android.os.Build;
 import android.os.Bundle;
-public class SplashScreenActivity extends Activity {
-    private final Handler mWaitHandler = new Handler();
+
+import androidx.annotation.RequiresApi;
+
+import java.util.Timer;
+import java.util.TimerTask;
+public class SplashScreenActivity extends MainActivity {
+    @RequiresApi(api = Build.VERSION_CODES.R)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-        mWaitHandler.postDelayed(() -> {
-            try {
-                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            public void run() {
+                Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
                 startActivity(intent);
                 finish();
-            } catch (Exception exception) {
-                exception.printStackTrace();
             }
         }, 2000);
-    }
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        mWaitHandler.removeCallbacksAndMessages(null);
     }
 }
